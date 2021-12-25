506f0f2a76ff4581b649324ad8f8c968b0159ea5
Theara10 <thearatheng10@gmail.com>
2021-12-25 16:36:42 +0700

    weather

--- src/projects/Weather.js
+++ src/projects/Weather.js
@@ -2,23 +2,67 @@ import React, { useEffect, useState } from "react";
 
 const api = {
   key: "bba0f44227592a2e1ef392aa581f1692",
-  base: "https://api.openweathermap.org/data/2.5",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 function Weather() {
-  const [query, setQuery] = useState("moscow");
+  const [query, setQuery] = useState("Moscow");
   const [weather, setWeather] = useState("");
 
-  useEffect(() => {
-    fetch(`${api.base}weather?q=${query}&unit=metric&APPID=${api.key}`)
-      .then((res) => res.json())
-      .then((x) => console.log(x));
-  }, []);
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((x) => {
+          setWeather(x);
+          setQuery("");
+          console.log(x.main.temp);
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
 
   return (
     <div className="weather">
       <h2>Weather App</h2>
       <div className="weather-content">
-        <input placeholder="Enter city name" />
+        <input
+          placeholder="Enter city name"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        />
         <div>
           <img
             src="https://www.freeiconspng.com/uploads/cloud-heavy-rain-weather-icon--3.png"
@@ -26,7 +70,7 @@ function Weather() {
             width="auto"
           />
 
-          <h3>32℃</h3>
+          <h3>{weather.main}℃</h3>
         </div>
       </div>
     </div>
