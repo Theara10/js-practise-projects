58ec64913029993f3f68f44ec530584ab43d3046
Theara10 <thearatheng10@gmail.com>
2022-02-13 20:05:48 +0700

    quiz

--- src/projects/Quiz.js
+++ src/projects/Quiz.js
@@ -1,16 +1,47 @@
 import React, { useState, useRef, useEffect, useContext } from "react";
 import { UserContext } from "../UserContext";
 
+const data = [
+  {
+    questionText: "Who is Luke Skywalker's father?",
+    answerOptions: [
+      { answerText: "Obiwan Kenobi", isCorrect: false },
+      { answerText: "Darth Vader", isCorrect: true },
+      { answerText: "Cad Bane", isCorrect: false },
+      { answerText: "Darth Maul", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Who is Batman?",
+    answerOptions: [
+      { answerText: "Bruce Wayne", isCorrect: false },
+      { answerText: "Harvey Dent", isCorrect: true },
+      { answerText: "Alfred", isCorrect: false },
+      { answerText: "Darth Maul", isCorrect: false },
+    ],
+  },
+];
+
 function Quiz() {
   const [input, setInput] = useState("");
-  const inputRef = useRef(null);
+  console.log(input);
 
   return (
     <div className="quiz">
       <div className="quiz-card">
         <h1>Quiz App</h1>
-        <h4>What is 1 + 1?</h4>
-        <input type="checkbox" placeholder="The answer is 2" name="name" />
+        {data.map((x) => (
+          <div>
+            <h4>{x.questionText}</h4>
+            <input
+              type="checkbox"
+              checked={true}
+              name={x.answerOptions.answerText}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <label>{x.answerOptions.answerText}</label>
+          </div>
+        ))}
       </div>
     </div>
   );
